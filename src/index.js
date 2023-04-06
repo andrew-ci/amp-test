179369a0dfbd9ea5e5891ba74769a49c0c1e3526
Andrew O'Regan <fantoraygun@gmail.com>
2023-04-06 13:54:04 +0100

    Amplify Studio Tutorial with react

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
+
+import { ThemeProvider } from '@aws-amplify/ui-react'
+import { studioTheme } from './ui-components'
+
+import '@aws-amplify/ui-react/styles.css'
+
+Amplify.configure(config)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <ThemeProvider theme={studioTheme}>
+      <App />
+    </ThemeProvider>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
